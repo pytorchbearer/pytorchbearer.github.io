298ed7ef223e25bd59162d3fb61513cc8d9bc9a4
Ethan Haris <ethanwharris@gmail.com>
2019-06-06 22:11:04 +0100

    Update

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,3 @@
-
 function scroll_to(clicked_link, nav_height) {
 	var element_class = clicked_link.attr('href').replace('#', '.');
 	var scroll_to = 0;
@@ -22,41 +21,11 @@ jQuery(document).ready(function() {
 		scroll_to($(this), $('nav').outerHeight());
 	});
 	// toggle "navbar-no-bg" class
-	$('.top-content .carousel').waypoint(function() {
-		$('nav').toggleClass('navbar-no-bg');
-	});
-
-    // /*
-    //     Background slideshow
-    // */
-    // $('.top-content').backstretch("assets/img/backgrounds/1.jpg");
-    // $('.call-to-action-container').backstretch("assets/img/backgrounds/1.jpg");
-    // $('.testimonials-container').backstretch("assets/img/backgrounds/1.jpg");
-    //
-    // $('#top-navbar-1').on('shown.bs.collapse', function(){
-    // 	$('.top-content').backstretch("resize");
-    // });
-    // $('#top-navbar-1').on('hidden.bs.collapse', function(){
-    // 	$('.top-content').backstretch("resize");
-    // });
-    //
-    // $('a[data-toggle="tab"]').on('shown.bs.tab', function(){
-    // 	$('.testimonials-container').backstretch("resize");
-    // });
-    //
-    // /*
-    //     Wow
-    // */
-    // new WOW().init();
+	$('.top-content .container').waypoint({
+    handler: function() {
+      $('nav').toggleClass('navbar-no-bg');
+      // $('nav').css("opacity: 0.66") 
+    }
+  });
 
 });
-
-
-// jQuery(window).load(function() {
-//
-// 	/*
-// 		Hidden images
-// 	*/
-// 	$(".testimonial-image img").attr("style", "width: auto !important; height: auto !important;");
-//
-// });
